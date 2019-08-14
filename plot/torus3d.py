a983a04d8b12e61cfd5d6408bd06c30aa64e08b3
wei zhang <wzhangpku@gmail.com>
2019-08-14 12:50:21 +0200

    pkgplot updated

--- plot/torus3d.py
+++ plot/torus3d.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import rc
  
+rc('font', size=28)
+rc('font', family='serif')
+rc('axes', labelsize=32)
 n = 100
  
 phi = np.linspace(0, 2 * np.pi, endpoint=True, num=n) 
@@ -19,11 +22,11 @@ x = np.cos(theta) * (R + r * np.cos(phi))
 y = np.sin(theta) * (R + r * np.cos(phi))
 z = r * np.sin(phi)
 z1 = (x**2 - (R + r)**2)**2
-z2 = z**2
+z2 = (x-R-r)**2
 N1 = z1 / z1.max()
 N2 = z2 / z2.max()
  
-fig = plt.figure(figsize=plt.figaspect(0.5), dpi=700)
+fig = plt.figure(figsize=plt.figaspect(0.25), dpi=700)
 #fig = plt.figure()
 ax1 = fig.add_subplot(1, 2, 1, projection='3d') 
 ax2 = fig.add_subplot(1, 2, 2, projection='3d') 
@@ -34,10 +37,16 @@ ax2.plot_surface(x, y, z, facecolors=plt.cm.jet(N2), rstride=1, cstride=1)
  
 lim = (max(abs(max(np.max(x), np.max(y), np.max(z))), abs(min(np.min(x), np.min(y), np.min(z))))) # find the absolute maximum of the function
 
+ax1.grid(True)
+ax1.xaxis.pane.set_edgecolor('black')
+ax1.yaxis.pane.set_edgecolor('black')
+ax1.xaxis.pane.fill = False
+ax1.yaxis.pane.fill = False
+ax1.zaxis.pane.fill = False
 ax1.set_xlim(-lim, lim)
 ax1.set_ylim(-lim, lim)
-ax1.set_zlim(-lim, lim)
-ax1.view_init(elev=30, azim=25)
+ax1.set_zlim(-2.5, 2.5)
+ax1.view_init(elev=40, azim=25)
 ax1.set_xlabel('$x$', fontsize=20, rotation=50)
 ax1.set_ylabel('$y$', fontsize=20, rotation=0)
 ax1.set_zlabel('$z$', fontsize=20, rotation=0)
@@ -48,10 +57,16 @@ ax1.tick_params(axis='x', pad=-3)
 ax1.tick_params(axis='y', pad=-5) 
 ax1.tick_params(axis='z', pad=-1) 
 
+ax2.grid(True)
+ax2.xaxis.pane.set_edgecolor('black')
+ax2.yaxis.pane.set_edgecolor('black')
+ax2.xaxis.pane.fill = False
+ax2.yaxis.pane.fill = False
+ax2.zaxis.pane.fill = False
 ax2.set_xlim(-lim, lim)
 ax2.set_ylim(-lim, lim)
 ax2.set_zlim(-lim, lim)
-ax2.view_init(elev=30, azim=25)
+ax1.view_init(elev=40, azim=25)
 ax2.set_xlabel('$x$', fontsize=20, rotation=50)
 ax2.set_ylabel('$y$', fontsize=20, rotation=0)
 ax2.set_zlabel('$z$', fontsize=20, rotation=0)
